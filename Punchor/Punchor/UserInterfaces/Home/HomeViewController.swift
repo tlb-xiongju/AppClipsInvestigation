778af0235b793bee588735752f70c630900f20c0
Xiong Ju <xiongju@team-lab.com>
2020-11-10 21:07:06 +0900

    feat: prepare a demo

---
+++ Punchor/Punchor/UserInterfaces/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Punchor
+//
+//  Created by 熊 炬 on 2020/11/10.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
