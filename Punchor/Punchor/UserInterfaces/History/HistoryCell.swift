254a9058b2aa5919c79160f6ae1ae02993d2d953
Xiong Ju <xiongju@team-lab.com>
2020-11-17 12:29:40 +0900

    views for a simple demo

---
+++ Punchor/Punchor/UserInterfaces/History/HistoryCell.swift
@@ -0,0 +1,10 @@
+//
+//  HistoryCell.swift
+//  Punchor
+//
+//  Created by 熊 炬 on 2020/11/17.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {}
---
+++ Punchor/Punchor/UserInterfaces/History/PagerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryViewController.swift
+//  Punchor
+//
+//  Created by 熊 炬 on 2020/11/10.
+//
+
+import UIKit
+
+class PagerViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
